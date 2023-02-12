5e05fad502a0d28479dd1580e0ed45b8c27a860b
zen <zhangyiming748@gmail.com>
2023-02-12 15:05:06 +0800

    暂时没想到连续提问的方法

--- ToOpenAI.go
+++ ToOpenAI.go
@@ -6,14 +6,11 @@ import (
 	"github.com/PullRequestInc/go-gpt3"
 	"github.com/zhangyiming748/log"
 	"strings"
-	"time"
 )
 
 var answer = ""
 
-func GetResponse(client gpt3.Client, ctx context.Context, quesiton string, out chan string) {
-	stop := make(chan bool)
-	go Wait(stop)
+func GetResponse(client gpt3.Client, ctx context.Context, quesiton string) string {
 	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
 		Prompt: []string{
 			quesiton,
@@ -28,55 +25,20 @@ func GetResponse(client gpt3.Client, ctx context.Context, quesiton string, out c
 		log.Warn.Panicf("%v\n", err)
 	}
 	log.Debug.Printf("%v\n", answer)
-	answer = <-out
-	log.Debug.Printf("%v\n", answer)
+	ans := answer
+	answer = ""
+	return ans
 }
 
 type NullWriter int
 
 func (NullWriter) Write([]byte) (int, error) { return 0, nil }
 
-func ChatGPT(key string, in chan string, out chan string) {
-	//log.SetOutput(new(NullWriter))
+func ChatGPT(key, ask string) string {
 	ctx := context.Background()
 	client := gpt3.NewClient(key)
-
-	for {
-		fmt.Print("still alive")
-		question := <-in
-		log.Debug.Printf("接收到了问题%v\n", question)
-		questionParam := validateQuestion(question)
-		GetResponse(client, ctx, questionParam, out)
-		if _, ok := <-in; !ok {
-			log.Warn.Println("程序退出")
-		}
-	}
-}
-
-// 这里判断问题内容是否有空格 如果有就截断 如果是关键词就返回空 这是什么用意
-func validateQuestion(question string) string {
-	quest := strings.Trim(question, " ")
-	keywords := []string{"", "loop", "break", "continue", "cls", "exit", "block"}
-	for _, x := range keywords {
-		if quest == x {
-			return ""
-		}
-	}
-	return quest
-}
-func Wait(ch chan bool) {
-	fmt.Printf("I see")
-	for {
-		time.Sleep(2 * time.Second)
-		fmt.Printf(".")
-		if <-ch {
-			close(ch)
-			break
-		}
-	}
-}
-
-// 如果收到退出信号 退出chatgpt程序
-func Exit(ch chan bool) {
-
+	fmt.Print("still alive")
+	question := ask
+	log.Debug.Printf("接收到了问题%v\n", question)
+	return GetResponse(client, ctx, question)
 }
--- gin.go
+++ gin.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/zhangyiming748/log"
+	"net/http"
 )
 
 type req struct {
@@ -10,42 +11,26 @@ type req struct {
 	Ask    string `json:"ask"`
 }
 
-var ()
-
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-	// Get stop
-	r.GET("/stop", func(c *gin.Context) {
-
-	})
 	r.POST("/chat", func(c *gin.Context) {
 		var json req
-
 		if err := c.BindJSON(&json); err != nil {
 			return
 		}
 		log.Info.Printf("%v", &json)
-
-		//in <- json.Ask
-		//log.Debug.Println("pass")
-		//c.JSON(http.StatusOK, gin.H{
-		//	"apikey": json.Apikey,
-		//	"ask":    json.Ask,
-		//	"ans":    <-out,
-		//})
+		ans := ChatGPT(json.Apikey, json.Ask)
+		c.JSON(http.StatusOK, gin.H{
+			"apikey": json.Apikey,
+			"ask":    json.Ask,
+			"ans":    ans,
+		})
 	})
-
 	return r
 }
 
 func main() {
-	in := make(chan string)
-	out := make(chan string, 1)
-	key := "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"
-	ChatGPT(key, in, out)
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
-
-// ToDo 单独接口设置key 使用循环阻塞拿到相应
